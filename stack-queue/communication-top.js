dd1ad8c68f6120a757cbd527bb2b3d258a041251
GilHyeon <gilhyeonhong@gmail.com>
2019-06-08 11:57:47 +0900

    프로그래머스 스택/큐 탑
    코드 리팩토링

--- stack-queue/communication-top.js
+++ stack-queue/communication-top.js
@@ -1,25 +1,13 @@
 // 문제 : https://programmers.co.kr/learn/courses/30/lessons/42588
 
 module.exports.solution = (heights) => {
-  const answer = [];
-  function findReceiver(receiveTop) {
-    if (receiveTop.length) {
-      // 신호를 수신한 탑을 반환합니다.
-      return receiveTop.pop().topNumber;
-    } else {
-      // 신호를 수신한 탑이 없으면 0을 반환합니다.
-      return 0;
-    }
-  }
-
-  heights.forEach((sender, index, srcArray) => {
+  return heights.map((sender, index, srcArray) => {
     // 신호를 수신할 수 있는 탑을 찾습니다.
     const receiveTop = srcArray
       .map((receiver, i) => (i < index && receiver > sender) ? {topNumber: i + 1, height: i} : null)
       .filter(e => e !== null);
 
     // 수신이 가능한 탑 중에서 가장 가까운 탑을 answer에 저장합니다.
-    answer.push(findReceiver(receiveTop));
+    return (receiveTop.length) ? receiveTop.pop().topNumber : 0;
   });
-  return answer;
 };
